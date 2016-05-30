d2ee8de5d4cbac8b554989879efe29625fcb0449
Administrator <Administrator@ROXATL7G3T4SVPD>
2016-05-30 22:02:08 +0800

    q

---
+++ src/test/java/com/corejava/multithread/v3/DivTask.java
@@ -0,0 +1,36 @@
+package com.corejava.multithread.v3;
+
+import java.util.concurrent.SynchronousQueue;
+import java.util.concurrent.ThreadPoolExecutor;
+import java.util.concurrent.TimeUnit;
+
+public class DivTask implements Runnable{
+
+	int a,b;
+	
+	public DivTask(int a, int b) {
+		super();
+		this.a = a;
+		this.b = b;
+	}
+
+	@Override
+	public void run() {
+
+		double re = a/b;
+		System.out.println(re);
+	}
+	
+	public static void main(String[] args) {
+		
+		ThreadPoolExecutor pools = new ThreadPoolExecutor(
+				0, 
+				Integer.MAX_VALUE, 
+				0, 
+				TimeUnit.SECONDS, new SynchronousQueue<Runnable>());
+		
+		for(int i=0;i<5;i++){
+			pools.execute(new DivTask(100, i));
+		}
+	}
+}
